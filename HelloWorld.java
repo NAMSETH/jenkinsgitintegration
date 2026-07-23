e7284ee9927007a9db9e25f96139d04693730936
NAMSETH <78471936+NAMSETH@users.noreply.github.com>
2026-07-23 09:04:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!tight");
+        System.out.println("Hello, World!tightkanda");
     }
 }
